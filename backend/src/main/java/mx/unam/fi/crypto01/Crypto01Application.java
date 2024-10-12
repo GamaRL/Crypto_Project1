0596133d88bc7fe79942768ce038a77f6773a2ec
GamaRL <gamarl1973@gmail.com>
2024-10-11 22:11:36 -0600

    first commit

---
+++ backend/src/main/java/mx/unam/fi/crypto01/Crypto01Application.java
@@ -0,0 +1,13 @@
+package mx.unam.fi.crypto01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crypto01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crypto01Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/mx/unam/fi/crypto01/Crypto01ApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.fi.crypto01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crypto01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
